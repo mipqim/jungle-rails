0d03567422241df9246a565358f916aec5cdc620
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-10-21 03:11:47 -0700

    Implement Admin Categories and remove category count by groupby

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,13 +2,41 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   def index
-    @product_count_category = Product.group(:category).count 
+    ########### TODO #####################################
+    ## HOW TO USE LEFT_OUTER_JOIN IN ACTIVERECORD? #######
+    ## CAN'T SEE A CATEGORY WHICH DOESN'T HAVE A PRODUCT #
+
+    ## CASE 1
+    # @product_count_category = Product.group(:category).count 
+    
+    ## OR CHANGE TO USE JUST 
+    ## CASE 2
+    @categories = Category.all
+    ## AND remove count on the view
+
+
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end    
   end
 
   def new
     @category = Category.new    
   end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end  
 end
