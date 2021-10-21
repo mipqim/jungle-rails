12f531fcaa50aea1f8c256b628a50e96ca503ebe
Sanghwan Jeon <jeff.shjeon@gmail.com>
2021-10-21 00:20:28 -0700

    Add showing categories on admin/categories page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
+    @product_count_category = Product.group(:category).count 
   end
 
   def create
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
-#   Display a count of how many products are in the database
-# Display a count of how many categories are in the database
-
   puts Product.count
   def show
     @product_count = Product.count
